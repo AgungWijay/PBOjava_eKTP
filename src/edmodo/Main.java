b262bdf9db6291c97459a2ca3adf9217b96dbf67
dedi <dedi@masdedot>
2017-05-18 20:28:48 +0700

    Fix

--- src/edmodo/Main.java
+++ src/edmodo/Main.java
@@ -23,31 +23,31 @@ import java.util.Map;
         	Map<String, Person> RT7p = new HashMap<>();
         	Map<String, Person> RT8p = new HashMap<>();
 
-        	//nambah orang------------------------------------------------------------------------------------------------
-            //int NIK ,nama,TTL,Aage,JK,Alamat,StatusKawin,Kewarganegaraan,Agama
-            Person jim = new Person(1,"jim","Balikpapan-8-Maret-1996",19, "L","Balikpapan", "BK","WNI","Islam");
-            Person scott = new Person(2,"Scott","Samarinda-3-Agustus-1995",21, "L","Balikpapan", "K","WNI","Islam");
-            Person anna = new Person(3,"anna","Samarinda-12-Agustus-1993",23, "P","Balikpapan", "K","WNI","Islam");
-            Person aanna = new Person(4,"aanna","Surabaya-30-Maret-1995",21, "P","Balikpapan", "BK","WNI","Islam");
-            Person ajim = new Person(5,"ajim","Surabaya-23-Maret-1995",21, "L","Balikpapan", "K","WNI","Islam");
-            Person dedi = new Person(6,"dedi","Trenggalek-6-Januari-1995",21, "L","Balikpapan", "K","WNI","Islam");
-            Person anggie = new Person(7,"anggie","Rejang Lebong-24-September-1995",21, "L","Balikpapan", "BK","WNI","Islam");
-            Person hussin = new Person(8,"hussin","Tulungagung-17-Januari-1995",21, "L","Balikpapan", "BK","WNI","Islam");
-            Person dani = new Person(9,"dani","Balikpapan-28-Januari-1992",24, "L","Balikpapan", "BK","WNI","Islam");
-            Person raden = new Person(10,"raden","curup-25-Januari-1995",21, "L","Balikpapan", "K","WNI","Islam");
-            Person purniawan = new Person(11,"purniawan","Trenggalek-28-Januari-1995",21, "L","Balikpapan", "BK","WNI","Islam");
-            Person ayu = new Person(12,"ayu","Jakarta-22-Juli-1998",18, "L","Balikpapan", "BK","WNI","Islam");
-            Person gista = new Person(13,"gista","Tanggerang-10-Maret-1999",17, "P","Balikpapan", "BK","WNI","Islam");
-            Person putri = new Person(14,"putri","Sidoarjo-4-April-1990",27, "P","Balikpapan", "K","WNI","Islam");
-            Person sugeng = new Person(15,"sugeng","Malang-20-Oktober-1995",21, "L","Balikpapan", "K","WNI","Islam");
-            Person parjo = new Person(16,"parjo","Sukabumi-19-November-1994",23, "L","Balikpapan", "BK","WNI","Islam");
-            Person sumiati = new Person(17,"sumiati","Surabaya-25-Agustus-1992",24, "P","Balikpapan", "K","WNI","Islam");
-            Person ucol = new Person(18,"ucol","Madiun-29-Mei-1990",27, "L","Balikpapan", "BK","WNI","Islam");
-            Person sulis = new Person(19,"sulis","Makassar-10-Maret-1991",26, "P","Balikpapan", "K","WNI","Islam");
-            Person ninda = new Person(20,"ninda","Trenggalek-12-Januari-1995",21, "P","Balikpapan", "BK","WNI","Islam");
+        	//nambah orang
+        	//int NIK ,nama,TTL,Age,JK,Alamat,StatusKawin,Kewarganegaraan,Agama,MasaBerlaku    
+            Person jim = new Person(1,"Jim","Balikpapan-8-Maret-1996",19,"L","Balikpapan","BK","WNI","8-Maret-2018","Islam");
+            Person scott = new Person(2,"Scott","Samarinda-3-Agustus-1995",21,"L","Balikpapan","K","WNI","3-Agustus-2018","Islam");
+            Person anna = new Person(3,"Anna","Samarinda-12-Agustus-1993",23,"P","Balikpapan","K","WNI","12-Agustus-2018","Islam");
+            Person aanna = new Person(4,"Aanna","Surabaya-30-Maret-1995",21,"P","Balikpapan","BK","WNI","30-Maret-2018","Islam");
+            Person ajim = new Person(5,"Ajim","Surabaya-23-Maret-1995",21,"L","Balikpapan","K","WNI","23-Maret-2018","Islam");
+            Person dedi = new Person(6,"Dedi","Trenggalek-6-Januari-1995",21,"L","Balikpapan","K","WNI","6-Januari-2018","Islam");
+            Person anggie = new Person(7,"Anggie","Rejang Lebong-24-September-1995",21,"L","Balikpapan", "BK","WNI","24-September-2018","Islam");
+            Person hussin = new Person(8,"Hussin","Tulungagung-17-Januari-1995",21,"L","Balikpapan","BK","WNI","17-Januari-2018","Islam");
+            Person dani = new Person(9,"Dani","Balikpapan-28-Januari-1992",24,"L","Balikpapan","BK","WNI","28-Januari-2018","Islam");
+            Person raden = new Person(10,"Raden","Curup-25-Januari-1995",21,"L","Balikpapan","K","WNI","25-Januari-2018","Islam");
+            Person purniawan = new Person(11,"Purniawan","Trenggalek-28-Januari-1995",21,"L","Balikpapan","BK","WNI","28-Januari-2018","Islam");
+            Person ayu = new Person(12,"Ayu","Jakarta-22-Juli-1998",18,"L","Balikpapan","BK","WNI","22-Juli-2018","Islam");
+            Person gista = new Person(13,"Gista","Tanggerang-10-Maret-1999",17,"P","Balikpapan","BK","WNI","10-Maret-2018","Islam");
+            Person putri = new Person(14,"Putri","Sidoarjo-4-April-1990",27,"P","Balikpapan","K","WNI","4-April-2018","Islam");
+            Person sugeng = new Person(15,"Sugeng","Malang-20-Oktober-1995",21,"L","Balikpapan","K","WNI","20-Oktober-2018","Islam");
+            Person parjo = new Person(16,"Parjo","Sukabumi-19-November-1994",23,"L","Balikpapan","BK","WNI","19-November-2018","Islam");
+            Person sumiati = new Person(17,"Sumiati","Surabaya-25-Agustus-1992",24,"P","Balikpapan","K","WNI","25-Agustus-2018","Islam");
+            Person ucol = new Person(18,"Ucol","Madiun-29-Mei-1990",27,"L","Balikpapan","BK","WNI","29-Mei-2018","Islam");
+            Person sulis = new Person(19,"Sulis","Makassar-10-Maret-1991",26,"P","Balikpapan","K","WNI","0-Maret-2018","Islam");
+            Person ninda = new Person(20,"Ninda","Trenggalek-12-Januari-1995",21,"P","Balikpapan","BK","WNI","12-Januari-2018","Islam");
           
 
-            //masukkan orang ke RT tertentu------------------------------------------------------------------------------------
+            //masukkan orang ke RT tertentu
             RT1p.put(jim.getNama(), jim);
             RT1p.put(scott.getNama(), scott);
             RT2p.put(anna.getNama(), anna);
@@ -68,7 +68,9 @@ import java.util.Map;
             RT8p.put(ucol.getNama(), ucol);
             RT8p.put(sulis.getNama(), sulis);
             RT8p.put(ninda.getNama(), ninda);
-            //Masukkin er Arraylist untuk sorting-------------------------------------------------------------------------
+            
+            
+            //Masukkin er Arraylist untuk sorting
             List<Person> RT1 = new ArrayList<>(RT1p.values());
             List<Person> RT2 = new ArrayList<>(RT2p.values());
             List<Person> RT3 = new ArrayList<>(RT3p.values());
@@ -79,81 +81,83 @@ import java.util.Map;
             List<Person> RT8 = new ArrayList<>(RT8p.values());
             
              
-            //sorting---------------------------------------------------------------------------------------------
-           Collections.sort(RT1, (Person o1, Person o2) -> o1.getAge() - o2.getAge());    
-    Collections.sort(RT2, (Person o1, Person o2) -> o1.getAge() - o2.getAge());
-    Collections.sort(RT3, (Person o1, Person o2) -> o1.getAge() - o2.getAge());
-    Collections.sort(RT4, (Person o1, Person o2) -> o1.getAge() - o2.getAge());
-    Collections.sort(RT5, (Person o1, Person o2) -> o1.getAge() - o2.getAge());
-    Collections.sort(RT6, (Person o1, Person o2) -> o1.getAge() - o2.getAge());
-    Collections.sort(RT7, (Person o1, Person o2) -> o1.getAge() - o2.getAge());
-    Collections.sort(RT8, (Person o1, Person o2) -> o1.getAge() - o2.getAge());
-            //Tampilkan ........................................................................................  
+            //sorting
+            Collections.sort(RT1, (Person o1, Person o2) -> o1.getAge() - o2.getAge());    
+		    Collections.sort(RT2, (Person o1, Person o2) -> o1.getAge() - o2.getAge());
+		    Collections.sort(RT3, (Person o1, Person o2) -> o1.getAge() - o2.getAge());
+		    Collections.sort(RT4, (Person o1, Person o2) -> o1.getAge() - o2.getAge());
+		    Collections.sort(RT5, (Person o1, Person o2) -> o1.getAge() - o2.getAge());
+		    Collections.sort(RT6, (Person o1, Person o2) -> o1.getAge() - o2.getAge());
+		    Collections.sort(RT7, (Person o1, Person o2) -> o1.getAge() - o2.getAge());
+		    Collections.sort(RT8, (Person o1, Person o2) -> o1.getAge() - o2.getAge());
             
+		    
+		    //menampilkan
             int i;
             System.out.println("DATA PENDUDUK KECAMATAN BALIKPAPAN BARAT");
             System.out.println("Daftar Kelurahan    :");
             System.out.println("1.Kelurahan Margo Mulyo\n2.Kelurahan Kampung Baru Ulu");
+            System.out.println("\nMasukkan Pilihan : ");
             i = input.nextInt();
             
-            // Kelurahan Margo Mulyo
+            //kelurahan Margo Mulyo
             	if(i==1) {
                 System.out.println("Daftar RT di Kelurahan Margo Mulyo:");
                 System.out.println("1.RT 1\n2.RT 2\n3.RT 3\n4.RT 4");
+                System.out.println("\nMasukkan Pilihan : ");
                 int rta = input.nextInt();
                 if(rta==1) {
                 	System.out.println("Daftar Penduduk : ");
                     RT1.forEach((p) -> {
-                    System.out.println(p.getNama() + "\t" + p.getAge()+"\t"+p.getStatusKawin()+"\t"+p.getAgama());
+                    System.out.println(p.getNIK()+"\t"+p.getNama()+"\t"+p.getTTL()+"\t"+p.getAge()+"\t"+p.getJenisKelamin()+"\t"+p.getAlamat()+"\t"+p.getStatusKawin()+"\t"+p.getKewarganegaraan()+"\t"+p.getMasaBerlaku()+"\t"+p.getAgama());
                 });              
                 }
                 else if(rta==2) {
                     RT2.forEach((p) -> {
-                    System.out.println(p.getNama() + "\t" + p.getAge()+"\t"+p.getStatusKawin()+"\t"+p.getAgama());
+                    System.out.println(p.getNIK()+"\t"+p.getNama()+"\t"+p.getTTL()+"\t"+p.getAge()+"\t"+p.getJenisKelamin()+"\t"+p.getAlamat()+"\t"+p.getStatusKawin()+"\t"+p.getKewarganegaraan()+"\t"+p.getMasaBerlaku()+"\t"+p.getAgama());
                 });
                 }
                 else if (rta==3) {
                     RT3.forEach((p) -> {
-                    System.out.println(p.getNama() + "\t" + p.getAge()+"\t"+p.getStatusKawin()+"\t"+p.getAgama());
-                });
+                    System.out.println(p.getNIK()+"\t"+p.getNama()+"\t"+p.getTTL()+"\t"+p.getAge()+"\t"+p.getJenisKelamin()+"\t"+p.getAlamat()+"\t"+p.getStatusKawin()+"\t"+p.getKewarganegaraan()+"\t"+p.getMasaBerlaku()+"\t"+p.getAgama());  
+                    });
                 }
                 else if(rta==4) {
                     RT4.forEach((p) -> {
-                    System.out.println(p.getNama() + "\t" + p.getAge()+"\t"+p.getStatusKawin()+"\t"+p.getAgama());
-                });
+                    System.out.println(p.getNIK()+"\t"+p.getNama()+"\t"+p.getTTL()+"\t"+p.getAge()+"\t"+p.getJenisKelamin()+"\t"+p.getAlamat()+"\t"+p.getStatusKawin()+"\t"+p.getKewarganegaraan()+"\t"+p.getMasaBerlaku()+"\t"+p.getAgama());  
+                    });
                 }   
             }
             	
-            // Kelurahan Kampung Baru Ulu
-                else if (i==2){ 
+            	
+            //kelurahan Kampung Baru Ulu
+                else if (i==2) { 
                 	System.out.println("Daftar RT di Kelurahan Kampung Baru Ulu");
                     System.out.println("1.RT 5\n2.RT 6\n3.RT 7\n4.RT 8");
+                    System.out.println("\nMasukkan Pilihan : ");
                     int rta = input.nextInt();
-                if(rta==1){
+                if(rta==1) { 
                     System.out.println("Daftar Penduduk : ");
                     RT5.forEach((p) -> {
-                    System.out.println(p.getNama() + "\t" + p.getAge()+"\t"+p.getStatusKawin()+"\t"+p.getAgama());
-                });              
+                    System.out.println(p.getNIK()+"\t"+p.getNama()+"\t"+p.getTTL()+"\t"+p.getAge()+"\t"+p.getJenisKelamin()+"\t"+p.getAlamat()+"\t"+p.getStatusKawin()+"\t"+p.getKewarganegaraan()+"\t"+p.getMasaBerlaku()+"\t"+p.getAgama());    
+                    });              
                 }
-                else if(rta==2){
+                else if(rta==2) {
                 	RT6.forEach((p) -> {
-                    System.out.println(p.getNama() + "\t" + p.getAge()+"\t"+p.getStatusKawin()+"\t"+p.getAgama());
-                });
+                	System.out.println(p.getNIK()+"\t"+p.getNama()+"\t"+p.getTTL()+"\t"+p.getAge()+"\t"+p.getJenisKelamin()+"\t"+p.getAlamat()+"\t"+p.getStatusKawin()+"\t"+p.getKewarganegaraan()+"\t"+p.getMasaBerlaku()+"\t"+p.getAgama());    
+                	});
                 }
                 else if (rta==3){
                     RT7.forEach((p) -> {
-                    System.out.println(p.getNama() + "\t" + p.getAge()+"\t"+p.getStatusKawin()+"\t"+p.getAgama());
-                });
+                    System.out.println(p.getNIK()+"\t"+p.getNama()+"\t"+p.getTTL()+"\t"+p.getAge()+"\t"+p.getJenisKelamin()+"\t"+p.getAlamat()+"\t"+p.getStatusKawin()+"\t"+p.getKewarganegaraan()+"\t"+p.getMasaBerlaku()+"\t"+p.getAgama());   
+                    });
                 }
                 else if(rta==4){
                     RT8.forEach((p) -> {
-                    System.out.println(p.getNama() + "\t" + p.getAge()+"\t"+p.getStatusKawin()+"\t"+p.getAgama());
-                });
+                    System.out.println(p.getNIK()+"\t"+p.getNama()+"\t"+p.getTTL()+"\t"+p.getAge()+"\t"+p.getJenisKelamin()+"\t"+p.getAlamat()+"\t"+p.getStatusKawin()+"\t"+p.getKewarganegaraan()+"\t"+p.getMasaBerlaku()+"\t"+p.getAgama());              
+                    });
                 }   
-                }
-            }       
-        }
-    
-	
-    
-
+          }
+     }       
+}
+        
--- src/edmodo/Person.java
+++ src/edmodo/Person.java
@@ -1,28 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edmodo;
-import java.util.Collections;
-/**
- *
- * @author Asus
- */
-public class Person //implements Comparable<Person>*
-{
-    private int NIK;
-     private String Nama;
-     private String TTL;
-      private String JenisKelamin;
-      private String Alamat;
-      private String StatusKawin;
-      private String Kewarganegaraan;
-      private String Agama;
-      private String MasaBerlaku;
-      
-     private int Age;
 
+import java.util.Collections;
+
+public class Person {
+    private int NIK;
+    private String Nama;
+    private String TTL;
+    private String JenisKelamin;
+    private String Alamat;
+    private String StatusKawin;
+    private String Kewarganegaraan;
+    private String Agama;
+    private String MasaBerlaku;
+    private int Age;
+
+    public Person(int NIK ,String nama,String TTL, int Age, String JK,String Alamat, String StatusKawin,String Kewarganegaraan,String MasaBerlaku,String Agama){
+        setNIK(NIK);
+        setNama(nama);
+        setTTL(TTL);
+        setAge(Age);
+        setJenisKelamin(JK);
+        setAlamat(Alamat);
+        setStatusKawin(StatusKawin);
+        setKewarganegaraan(Kewarganegaraan);
+        setMasaBerlaku(MasaBerlaku);
+        setAgama(Agama);
+    }
+    
     public int getAge() {
         return Age;
     }
@@ -30,31 +34,15 @@ public class Person //implements Comparable<Person>*
     public void setAge(int Age) {
         this.Age = Age;
     }
-	
-       
-	public Person(int NIK ,String nama,String TTL, int Aage, String JK,String Alamat, String StatusKawin,String Kewarganegaraan, String Agama){
-            setNIK(NIK);
-            setNama(nama);
-                setTTL(TTL);
-		setAge(Aage);
-                setJenisKelamin(JK);
-                setAlamat(Alamat);
-                setStatusKawin(StatusKawin);
-                setKewarganegaraan(Kewarganegaraan);
-                setAgama(Agama);
-
-	
-	}
-
-        public int getNIK() {
-        return NIK;
+	   
+    public int getNIK() {
+    	return NIK;
     }
 
     public void setNIK(int NIK) {
         this.NIK = NIK;
     }
    
-
     public String getNama() {
         return Nama;
     }
@@ -62,27 +50,30 @@ public class Person //implements Comparable<Person>*
     public void setNama(String Nama) {
         this.Nama = Nama;
     }
-        public String getTTL() {
-        return Nama;
+    
+    public String getTTL() {
+    	return TTL;
     }
 
     public void setTTL(String TTL) {
         this.TTL = TTL;
     }
-      public String getStatusKawin() {
+    
+    public String getStatusKawin() {
         return StatusKawin;
     }
-       public String getAlamat() {
+    
+    public void setStatusKawin(String StatusKawin) {
+        this.StatusKawin = StatusKawin;
+    }
+    
+    public String getAlamat() {
         return Alamat;
     }
 
     public void setAlamat(String Alamat) {
         this.Alamat = Alamat;
-    }
-
-    public void setStatusKawin(String StatusKawin) {
-        this.StatusKawin = StatusKawin;
-    }
+    } 
 
     public String getJenisKelamin() {
         return JenisKelamin;
@@ -91,13 +82,15 @@ public class Person //implements Comparable<Person>*
     public void setJenisKelamin(String JenisKelamin) {
         this.JenisKelamin = JenisKelamin;
     }
-     public String getKewarganegaraan() {
+    
+    public String getKewarganegaraan() {
         return Kewarganegaraan;
     }
 
     public void setKewarganegaraan(String Kewarganegaraan) {
         this.Kewarganegaraan = Kewarganegaraan;
     }
+    
     public String getAgama() {
         return Agama;
     }
@@ -105,9 +98,11 @@ public class Person //implements Comparable<Person>*
     public void setAgama(String Agama) {
         this.Agama = Agama;
     }
-    public String getMasaBerlaku(String MasaBerlaku) {
+    
+    public String getMasaBerlaku() {
         return MasaBerlaku;
     }
+    
     public void setMasaBerlaku(String MasaBerlaku) {
         this.MasaBerlaku = MasaBerlaku;
     }
